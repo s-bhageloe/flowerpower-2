1ee0a64a338191f18b9978e2afd296e07ab7e357
s-bhageloe <2117940@talnet.nl>
2022-04-16 18:46:14 +0200

    flowerpower 2

---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+class DB{
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
+    private $charset;
+    private $pdo;
+
+    public function __construct($host, $user, $pass, $db, $charset){
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db = $db;
+        $this->charset = $charset; 
+        
+     
+
+        try{
+            $dsn = 'mysql:host='. $this->host.';dbname='.$this->db.';charset='.$this->charset;
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $this->pdo;
+        }
+        catch(\PDOException $e){
+            echo "Connection Failed: ".$e->getMessage();
+        }
+      
+    }
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+
+  <!--<div class="verticalbar">
+    <a href="index.php"><img src="boeket.png" width="190" height="190"></a>
+    <a href="loginEmployee.php">Inloggen medewerkers</a>
+    <a href="loginCustomer.php">Inloggen klanten</a>
+    <a href="contact.php">Contactpagina</a>
+    <a href="register.php">Registreren</a>
+  </div>
+-->
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="index.php">
+    <img src="boeket.png" alt="logo" style="width:40px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="loginEmployee.php">Inloggen klanten</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="loginCustomer.php">Inloggen medewerkers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="contact.php">Contactpagina</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="register.php">Registreren</a>
+    </li>
+  </ul>
+</nav>
+
+  <div id="horizontal">
+    <div class="bar">
+  </div>
+
+  <div class="body">
+    <h2>Flowerpower</h2>
+    <h4>Welkom op de FlowerPower site!</h4>
+  </div>
+
+</body>
+</html>
